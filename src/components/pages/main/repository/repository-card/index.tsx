8261029e6a619c1f6f860cd8a80fbce4bc03ea7a
Adi Wahyudi <adi.whyd02@gmail.com>
2025-02-28 17:54:47 +0700

    feat: create repository-card component

---
+++ src/components/pages/main/repository/repository-card/index.tsx
@@ -0,0 +1,50 @@
+import { updatedOnFormatDate } from "@/utils/date.utils";
+import React from "react";
+import { IoEyeOutline } from "react-icons/io5";
+
+interface IRepositoryCardProps {
+  repoName: string;
+  repoUrl: string;
+  description: string | null;
+  language: string | null;
+  watchers: number;
+  updatedAt: string;
+}
+
+export const RepositoryCard: React.FC<IRepositoryCardProps> = ({
+  repoName,
+  repoUrl,
+  description,
+  language,
+  watchers,
+  updatedAt,
+}) => {
+  return (
+    <div className="bg-gray-800 text-white rounded-lg p-4 mb-2 w-full shadow-md">
+      <a
+        href={repoUrl}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="text-blue-400 text-xl font-semibold hover:underline cursor-pointer"
+      >
+        {repoName}
+      </a>
+      <p className="text-gray-400 mt-1">{description}</p>
+      <div className="flex items-center justify-between flex-wrap gap-4 text-sm text-gray-400 mt-4">
+        <div className="flex items-center gap-4">
+          <span className="flex items-center gap-1">
+            <IoEyeOutline />
+            {watchers.toLocaleString()}
+          </span>
+          {!!language && (
+            <span className="flex items-center gap-1">
+              <span>●</span>
+              {language}
+            </span>
+          )}
+        </div>
+        <span>Updated on {updatedOnFormatDate(updatedAt)}</span>
+      </div>
+    </div>
+  );
+};
