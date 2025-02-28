8a1a1ffcc50f7c7c0eb6319a72d2b9f3b1712506
Adi Wahyudi <adi.whyd02@gmail.com>
2025-02-28 17:55:28 +0700

    feat: create repository list component

---
+++ src/components/pages/main/repository/repository-list/index.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { useUserRepositoryContext } from "@/contexts/user-repository.context";
+import { RepositoryCard } from "../../repository/repository-card";
+import { RepositoryCardPlaceholder } from "../../repository/repository-card-placeholder";
+import { Message } from "@/components/ui/message";
+
+export const RepositoryList: React.FC = () => {
+  const {
+    repositories,
+    isLoadingRepositories,
+    isFetchingNextRepositories,
+    hasNextPageRepositories,
+    paramsGetRepositories,
+    fetchNextPageRepositories,
+  } = useUserRepositoryContext();
+
+  return (
+    <>
+      {repositories.map((repo) => (
+        <RepositoryCard
+          key={repo.id}
+          repoName={repo.full_name}
+          repoUrl={repo.html_url}
+          description={repo.description}
+          language={repo.language}
+          watchers={repo.watchers}
+          updatedAt={repo.updated_at}
+        />
+      ))}
+
+      <RepositoryCardPlaceholder
+        count={paramsGetRepositories.per_page}
+        isVisible={isLoadingRepositories || isFetchingNextRepositories}
+      />
+
+      {!repositories.length &&
+        !hasNextPageRepositories &&
+        !isFetchingNextRepositories &&
+        !isLoadingRepositories && (
+          <Message message="This user hasn’t shared any repositories yet. Check back later or explore other users." />
+        )}
+
+      {hasNextPageRepositories && (
+        <button
+          role="button"
+          onClick={() => fetchNextPageRepositories()}
+          disabled={!hasNextPageRepositories || isFetchingNextRepositories}
+          className="w-full bg-[#0d1117] border border-[#30363d] rounded-md py-2 text-sm text-white hover:bg-[#161b22] transition duration-300 ease-in-out cursor-pointer disabled:bg-[#161b22] disabled:text-gray-500 disabled:cursor-not-allowed"
+        >
+          {isLoadingRepositories ? "Loading more..." : "Load More"}
+        </button>
+      )}
+    </>
+  );
+};
