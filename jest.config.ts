6231e48e79f53abc3407a4c4791124df1daeae4c
Adi Wahyudi <adi.whyd02@gmail.com>
2025-02-28 18:00:24 +0700

    chore: add dependencies and setup for testing

---
+++ jest.config.ts
@@ -0,0 +1,42 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+
+  preset: "ts-jest",
+
+  testEnvironment: "jsdom",
+
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": [
+      "babel-jest",
+      {
+        presets: [
+          "@babel/preset-env",
+          "@babel/preset-react",
+          "@babel/preset-typescript",
+        ],
+      },
+    ],
+  },
+
+  transformIgnorePatterns: [
+    "/node_modules/(?!YOUR_MODULE_NAME)", // Adjust YOUR_MODULE_NAME if needed
+    "\\.pnp\\.[^\\/]+$",
+  ],
+
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+    "next/font/google": "<rootDir>/__mocks__/next/font/google.js",
+  },
+
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json"],
+
+  testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[tj]s?(x)"],
+};
+
+export default config;
